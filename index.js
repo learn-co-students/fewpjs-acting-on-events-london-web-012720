4269cf2476dcc8d24d2cde3fa7b8a16f7eb0157f
RikBala <rik280491@googlemail.com>
2020-03-09 15:13:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+
+
+
+    const moveDodgerLeft = () => {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+    const moveDodgerRight = () => {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+      dodger.style.left = `${left + 1}px`;
+    }
