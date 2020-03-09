4060118943bd61c6c571131bcd79265f72273032
alexandre jamarco <alexandre.j@gmail.com>
2020-03-09 13:26:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+dodger = document.getElementById('dodger');
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+    else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    } 
+  });
+
+function moveDodgerLeft(){
+    let position = dodger.style.left.replace("px","");
+    position = parseInt(position, 10);
+    if (position > 0){
+        dodger.style.left = `${position-1}px`;
+    }
+    console.log(dodger.style.left);
+}
+
+function moveDodgerRight(){
+    let width = dodger.offsetWidth;
+    let position = dodger.style.left.replace("px","");
+    position = parseInt(position, 10);
+    if (position + width < 400){
+        dodger.style.left = `${position+1}px`;
+    }
+}
